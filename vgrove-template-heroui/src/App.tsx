4b20401f28afde997f028f2a81906e8a3dae70ae
RHectorGu <gjwmaster2024@163.com>
2025-06-15 09:34:49 +0800

    更新模版

--- vgrove-template-heroui/src/App.tsx
+++ vgrove-template-heroui/src/App.tsx
@@ -2,7 +2,7 @@ import { createI18nClient, I18nProvider } from '@gulibs/react-autoroutes-client'
 import { RouterProvider, createBrowserRouter } from 'react-router';
 import { resources, supportedLocales } from '@gulibs/i18n-locales';
 import { routes } from '@gulibs/react-autopages';
-console.log("routes", routes)
+
 const router = createBrowserRouter(routes);
 
 const client = createI18nClient({
